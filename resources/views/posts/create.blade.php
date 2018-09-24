d7923d05ad79fd163d7d426d99452d40d9f7cfd6
zayrtun <zay.t@hotmail.com>
2018-09-24 18:04:24 +0630

    Initial Commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="col-sm-8 blog-main">
+
+        <h1>Create new Post</h1>
+
+        <hr>
+
+        <form action="/posts" method="POST">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="title">Title:</label>
+                <input name="title" type="text" class="form-control" id="title" aria-describedby="Post Title">
+            </div>
+
+            <div class="form-group">
+                <label for="body">Body:</label>
+                <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Publish</button>
+
+        </form>
+
+    </div>
+@endsection
